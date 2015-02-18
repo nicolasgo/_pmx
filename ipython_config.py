23f93d77d315a7f5abd34660db0bc6e1b57c8630
nicolasgo <Nicolas@Pragmex.local>
2015-02-18 16:25:39 -0500

    New findings -- videos

--- ipython_config.py
+++ ipython_config.py
@@ -14,15 +14,10 @@ sns.set_palette("deep", desat=.6)
 sns.set_context(rc={"figure.figsize": (8, 4)})
 
 
-# Make graphs prettier
-pd.set_option('display.width', 400)
-pd.set_option('display.line_width', 4)
-pd.set_option('display.max_columns', 15)
-pd.set_option('display.mpl_style', 'default')
-
 # Make the fonts bigger
-matplotlib.rc('figure', figsize=(8, 5))
-matplotlib.rc('font', family='normal', weight='bold', size=18)
+#import matplotlib
+#matplotlib.rc('figure', figsize=(8, 5))
+#matplotlib.rc('font', family='normal', weight='bold', size=18)
 
 print 'configured as per my config'
 print '*** ++out config'
