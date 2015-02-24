13088ad88add7d4dae0386b302ea2055ccac6560
nicolasgo <nicolas@NicolasGo.local>
2015-02-24 10:13:07 -0500

    crypto

--- code/crypto.py
+++ code/crypto.py
@@ -1,5 +1,9 @@
-'''module for handling encryption
-   Created by Nicolas Gosselin
+'''
+ module for encrypting elements
+   
+ Copyright (c) 2014, 2015, Pragmex Inc, All Right Reserved
+ http://pragmex.com/
+    
 '''
 
 import hashlib
@@ -7,18 +11,45 @@ import hashlib
 saved = {}
 rsaved = {}  # a reverse dictionary can be used to lookup original msisdn from hashed ids
 
-def xx(x):
-    if not saved.has_key(x):
+
+def xx_msisdn(msisdn):
+    if type(msisdn)==int:
+        msisdn=str(msisdn)
+    if not saved.has_key(msisdn):
         try:
             m = hashlib.md5()
-            m.update(x)
+
+            m.update(msisdn)
             mh = '%.12s'%(m.hexdigest()[10:30])
-            saved[x]=mh
-            rsaved[mh]=x
+            
+            saved[msisdn]=mh
+            rsaved[mh]=msisdn
 
         except:
-            print type(x),x
+            print type(msisdn),msisdn
 
-    return saved[x]
+    return saved[msisdn]
+    
 
+def xx_ip(ip_address):
+    y='1.2.3.4'
+    try:
+        parts=[]
+        extra=0
+        for elem in ip_address.split('.'):
+#            print elem,int(elem)^(0xA8+extra),int(elem)^(0xA8+extra*7)
+            parts.append(int(elem)^(0xA8+extra*7))
+            extra +=1
+        y=''
+#        print ip_address,parts[::-1]
+        for p in parts[::-1]:
+            y+='%x.'%(p)
+#            print y
+        
+        y=y[:-1] # remove the last '.'
+
+    except:
+        print type(ip_address),ip_address
+
+    return y
 
