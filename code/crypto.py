beece2bc9bfa3ecba8228c7ec5f593c00a8aa079
nicolasgo <Nicolas@Pragmex.local>
2015-02-24 21:16:58 -0500

    ...

--- code/crypto.py
+++ code/crypto.py
@@ -8,31 +8,31 @@
 
 import hashlib
 
-saved = {}
-rsaved = {}  # a reverse dictionary can be used to lookup original msisdn from hashed ids
+xx_saved = {}
+xx_rsaved = {}  # a reverse dictionary can be used to lookup original msisdn from hashed ids
 
 
 def xx_msisdn(msisdn):
     if type(msisdn)==int:
         msisdn=str(msisdn)
-    if not saved.has_key(msisdn):
+    if not xx_saved.has_key(msisdn):
         try:
             m = hashlib.md5()
 
             m.update(msisdn)
             mh = '%.12s'%(m.hexdigest()[10:30])
             
-            saved[msisdn]=mh
-            rsaved[mh]=msisdn
+            xx_saved[msisdn]=mh
+            xx_rsaved[mh]=msisdn
 
         except:
             print type(msisdn),msisdn
 
-    return saved[msisdn]
+    return xx_saved[msisdn]
     
 
-def xx_ip(ip_address):
-    y='1.2.3.4'
+def xx_ip(ip_address, hexa=False):
+    y='-'
     try:
         parts=[]
         extra=0
@@ -43,13 +43,17 @@ def xx_ip(ip_address):
         y=''
 #        print ip_address,parts[::-1]
         for p in parts[::-1]:
-            y+='%x.'%(p)
+            if hexa:
+                y+='%0  x.'%(p)
+            else:
+                y+='%d.'%(p)
 #            print y
         
         y=y[:-1] # remove the last '.'
 
     except:
-        print type(ip_address),ip_address
+#        print type(ip_address),ip_address
+        pass
 
     return y
 
